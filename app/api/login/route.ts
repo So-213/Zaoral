54318768f50c27800290ee113316c828bb0782ab
So-213 <betepp7@gmail.com>
2025-03-02 14:35:42 +0900

    vercelサーバ活用4

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
 
-
-
-
 export async function GET() {
   try {
     const backendUrl = "https://zrl213.com/login";
@@ -10,20 +7,21 @@ export async function GET() {
     // バックエンドにリクエストを送る
     const response = await fetch(backendUrl, {
       method: "GET",
-      credentials: "include", // クッキーを含める
+      credentials: "include",// クッキーを含める
     });
 
     if (!response.ok) {
+      console.error("🛑 バックエンドの /login がエラー", await response.text());
       return NextResponse.json({ error: "Login failed" }, { status: 500 });
     }
 
     // バックエンドがリダイレクトするURLを取得      //cognito認証ページはバックエンドしかわからないのでそれをfetchしてくる
-    const redirectUrl = response.url;
-
-    return NextResponse.json({ redirectUrl });
-    
+    // const data = response.url;
+    const data = await response.json();
+    return NextResponse.json(data);
   } catch (error) {
-    console.error("Error in /api/login:", error);
+    console.error("❌ Next.js /api/login のエラー:", error);
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
+
