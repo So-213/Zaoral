f5251ca8fb4eb0b7e6ea95d1cf830b311ef19465
So-213 <betepp7@gmail.com>
2025-03-23 15:16:55 +0900

    ナブバー1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,21 @@
 import { SessionProvider } from "next-auth/react";
 import type { ReactNode } from "react";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <SessionProvider>
+          <Navbar />
           {children}
         </SessionProvider> 
       </body>
     </html>
   );
 }
+
+
+
+
