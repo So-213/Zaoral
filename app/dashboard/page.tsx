7121ed054fea114bd9e578fcbdfd6877e54ccbd2
So-213 <betepp7@gmail.com>
2025-03-26 10:58:21 +0900

    auth5実装by公式doc2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,14 +3,6 @@ import { auth } from "@/lib/auth";
 export default async function DashboardPage() {
   const session = await auth();
 
-  if (!session) {
-    return <p>認証が必要です</p>;
-  }
+ return <p>ようこそ、{session!.user?.name} さん！</p>;
 
-
-
-
-
-  
-  return <p>ようこそ、{session.user?.name} さん！</p>;
 }
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,16 @@
-export { auth as middleware } from "@/lib/auth"
+import { auth } from "@/lib/auth";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+    const session = await auth();
+
+    if (!session) {
+        // ログインしていない場合は `/login` にリダイレクト
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: "/dashboard/:path*",
+};
